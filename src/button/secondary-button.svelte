b63d30c6ee7559ae6f58dec87339bc9218a77b31
gayashan4lk <gayashan4lk@gmail.com>
2025-12-03 19:00:43 +0530

    secondary button styles

--- src/button/secondary-button.svelte
+++ src/button/secondary-button.svelte
@@ -2,4 +2,8 @@
   const { label = 'Button', ...props } = $props();
 </script>
 
-<button {...props}>{label}</button>
+<button
+  {...props}
+  class="cursor-pointer rounded-md bg-blue-300 px-2 text-blue-950 hover:bg-blue-400 hover:shadow-md"
+  >{label}</button
+>
