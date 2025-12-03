57fa841c23aefcb4eebb6a0dd88cc4c7940504a5
gayashan4lk <gayashan4lk@gmail.com>
2025-12-03 19:02:35 +0530

    danger button markup

--- src/button/danger-button.svelte
+++ src/button/danger-button.svelte
@@ -2,4 +2,8 @@
   const { label = 'Button', ...props } = $props();
 </script>
 
-<button {...props}>{label}</button>
+<button
+  {...props}
+  class="cursor-pointer rounded-md bg-rose-500 px-2 text-white hover:bg-rose-700 hover:shadow-md"
+  >{label}</button
+>
