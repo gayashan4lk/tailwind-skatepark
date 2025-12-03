4df3b34d8ef5613c8f1e6a87a170e0cfa0b35d3a
gayashan4lk <gayashan4lk@gmail.com>
2025-12-03 15:50:21 +0530

    primary button

--- src/button/primary-button.svelte
+++ src/button/primary-button.svelte
@@ -2,4 +2,9 @@
   const { label = 'Button', ...props } = $props();
 </script>
 
-<button {...props}>{label}</button>
+<button
+  {...props}
+  class="cursor-pointer rounded-md bg-blue-500 px-2 text-white hover:bg-blue-600 hover:shadow-md"
+>
+  {label}
+</button>
