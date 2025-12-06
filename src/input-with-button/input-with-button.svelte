c40a97ed244ee28f3394c32bad13123b7f447a5a
gayashan4lk <gayashan4lk@gmail.com>
2025-12-07 01:38:49 +0530

    styling input with button

--- src/input-with-button/input-with-button.svelte
+++ src/input-with-button/input-with-button.svelte
@@ -1,5 +1,5 @@
-<form>
-  <label for="new-task">What do you need to do?</label>
-  <input type="text" id="new-task" placeholder="New task…" />
-  <button>Submit</button>
+<form class="flex rounded p-2 outline outline-slate-600 focus-within:outline-2 focus-within:outline-purple-500">
+  <label for="new-task" class="sr-only">New Task</label>
+  <input type="text" id="new-task" placeholder="New task…" class="flex-1 outline-none" />
+  <button class="rounded bg-purple-500 px-2 py-1 text-white">Submit</button>
 </form>
