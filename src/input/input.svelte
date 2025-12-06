df5ee17b97d26a5d06055e2e7d3ca16db9ba8c95
gayashan4lk <gayashan4lk@gmail.com>
2025-12-06 14:25:20 +0530

    input styling

--- src/input/input.svelte
+++ src/input/input.svelte
@@ -4,14 +4,15 @@
   const errorId = error ? `${id}-error` : undefined;
 </script>
 
-<div>
-  <label for={id}>{label}</label>
+<div class="space-y-1.5">
+  <label for={id} class="text-md block font-semibold text-slate-900">{label}</label>
   <input
     {id}
     aria-describedby={descriptionId}
     aria-invalid={!!error}
     aria-errormessage={errorId}
     {...props}
+    class="block w-full rounded-md bg-slate-50 px-2 py-1 outline-2 outline-slate-200 focus:outline-2 focus:outline-blue-400"
   />
   {#if description}
     <p id={descriptionId}>{description}</p>
