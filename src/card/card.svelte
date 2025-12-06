a09b6a010afb9c8c168e9bfdc415133750141496
gayashan4lk <gayashan4lk@gmail.com>
2025-12-07 03:04:33 +0530

    dark mode

--- src/card/card.svelte
+++ src/card/card.svelte
@@ -2,7 +2,7 @@
   const { title = 'Card Title', description = 'Card description' } = $props();
 </script>
 
-<div class="w-[300px] rounded-md bg-teal-400 p-4 pt-20">
-  <h2 class="mb-1 font-serif text-3xl font-bold antialiased">{title}</h2>
-  <p class="mt-1 text-xs font-light antialiased">{description}</p>
+<div class="w-[300px] rounded-md bg-teal-400 p-4 pt-20 dark:bg-teal-600">
+  <h2 class="mb-1 font-serif text-3xl font-bold antialiased dark:text-slate-100">{title}</h2>
+  <p class="mt-1 text-xs font-light antialiased dark:text-slate-100">{description}</p>
 </div>
